4c9399b895968e6b3773e939c9debb63bd050061
komisuomi <only-for-serzh@yandex.ru>
2020-11-21 20:26:05 +0300

    Test added (failed)

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -3,20 +3,22 @@ package ru.netology.rest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
     @Test
     void shouldReturnPostmanEcho() {
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data")
+                .contentType("text/plain")
+                .body("London is the capital of GB")
 
                 .when()
                 .post("/post")
 
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("London is the capital"))
         ;
     }
 }
